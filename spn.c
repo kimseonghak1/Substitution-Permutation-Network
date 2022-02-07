72fc986b869b4f06a15500fb9ea86284cb14c3e6
seonghak2 <jip9821@cu.ac.kr>
2022-02-07 17:03:35 +0900

    완전히 끝냄
    함수 합친 것도 굿.
    하나씩 해본 거랑 일치

--- spn.c
+++ spn.c
@@ -1,42 +1,30 @@
-// 비선형 S-box, 선형 P-box
-#include <stdio.h>
-
+#include <stdio.h> //[SPN구조를 이용] 주어진 평문1,평문2가 1비트만 달라도  S-Box (o) = (완전 다른 값) but, S-Box (x), P-Box만(o) = (1비트만 달라짐)
+int plaintext0 = 0x1234;
 int plaintext = 0xABCD; // 16비트
 int Key1[5] = { 0x1234,0x2345,0x3456,0x4567,0x5678 }; // 16비트
 
-int plaintext1 = 0xD178; // 0x7563, 0xD178;
-int plaintext2 = 0xC178; //0x7562, 0xC178;
+int plaintext1 = 0xD178; // BIN { 0,1,1,1,0,1,0,1,0,1,1,0,0,0,1,1 } 011
+int plaintext2 = 0xC178; // BUN { 0,1,1,1,0,1,0,1,0,1,1,0,0,0,1,0 }
 
-int sBox1[4][16] = {
-    {14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},
-    {0,15,7,4,14,2,13,10,3,6,12,11,9,5,3,8},
-    {4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0},
-    {15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13}
+int sBox1[16] = {
+   14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7
 };
 
 int pBox1[16] = { 1,5,9,13,2,6,10,14,3,7,11,15,4,8,12,16 };
 
-void substitute(int n, int inBlock[16], int outBlock[16], int sBox1[4][16]) { // 4bit -> 4bit
+void substitute(int inBlock[16], int outBlock[16], int sBox1[16]) { // 4bit -> 4bit
     int col, value;
     for (int i = 0; i < 4; i++) {
         col = 8 * inBlock[i * 4 + 0] + 4 * inBlock[i * 4 + 1] + 2 * inBlock[i * 4 + 2] + 1 * inBlock[i * 4 + 3];
+        value = sBox1[col];
 
-        value = sBox1[n][col];
-
-        outBlock[i * 4] = value / 8; 
-        value = value % 8; 
-        outBlock[i * 4 + 1] = value / 4; 
-        value = value % 4; 
-        outBlock[i * 4 + 2] = value / 2; 
-        value = value % 2; 
-        outBlock[i * 4 + 3] = value; 
-        //outBlock[i * 4 + 3] = value % 2;
-        //value = value / 2;
-        //outBlock[i * 4 + 2] = value % 2;
-        //value = value / 2;
-        //outBlock[i * 4 + 1] = value % 2;
-        //value = value / 2;
-        //outBlock[i * 4] = value % 2;
+        outBlock[i * 4 + 3] = value % 2;
+        value = value / 2;
+        outBlock[i * 4 + 2] = value % 2;
+        value = value / 2;
+        outBlock[i * 4 + 1] = value % 2;
+        value = value / 2;
+        outBlock[i * 4 + 0] = value % 2;
     }
 }
 
@@ -52,49 +40,68 @@ void exclusiveOr(int text[], int key[], int XORtext[]) { // XOR
     }
 }
 
-void mixer(int n, int inBlock[16], int outPBlock[16], int KeyBlock[16]) {
-    int mixingtext[16], outBlock[16];
-    exclusiveOr(inBlock, KeyBlock, mixingtext);
-    substitute(n,mixingtext, outBlock, sBox1);
-    permute(outBlock, outPBlock, pBox1);
+void mixer(int inBlock[16], int outPBlock[16], int KEYBlock[16]) {
+    int outBlock[16] = { 0, }, outSBlock[16] = { 0, };
+    exclusiveOr(inBlock, KEYBlock, outBlock);
+    substitute(outBlock, outSBlock, sBox1);
+    permute(outSBlock, outPBlock, pBox1);
 }
 
-void mixer1(int n, int inBlock[16], int outPBlock[16], int KeyBlock[16]) {
+void mixer1(int inBlock[16], int outPBlock[16], int KEYBlock[16]) {
     int mixingtext[16], outBlock[16];
-    exclusiveOr(inBlock, KeyBlock, mixingtext);
+    exclusiveOr(inBlock, KEYBlock, mixingtext);
     //substitute(mixingtext, outBlock, sBox1);
     permute(mixingtext, outPBlock, pBox1);
 }
 
 void SPNcihper(int plaintext[16], int KeyBlock[5][16], int ciphertext[16]) {
-    int mixingtext[16], outBlock[16], outPBlock[16], lastBlock[16];
-    for (int round = 0; round < 4; round++) {
-        mixer(round, plaintext, outPBlock, KeyBlock[round]); // round 1
-        if (round > 0 && round < 3) { // round 2,3
-            mixer(round, outPBlock, outBlock, KeyBlock[round]);
+    int outBlock[16] = { 0, }, mixingBlock[16] = { 0, }, lastBlock[16] = { 0, };
+    for (int round = 0; round < 5; round++) {
+        if (round == 0) {
+            mixer(plaintext, mixingBlock, KeyBlock[round]);
         }
-        if (round == 3) { // round 4
-            exclusiveOr(outBlock, KeyBlock[round], mixingtext);
-            substitute(round, mixingtext, lastBlock, sBox1);
+        if (round > 0 && round < 3) {
+            mixer(mixingBlock, mixingBlock, KeyBlock[round]);
+        }
+        if (round == 3) {
+            exclusiveOr(mixingBlock, KeyBlock[round], outBlock);
+            substitute(outBlock, lastBlock, sBox1);
+        }
+        if (round == 4) {
+            exclusiveOr(lastBlock, KeyBlock[4], ciphertext); 
         }
     }
-    exclusiveOr(lastBlock, KeyBlock[4], ciphertext); // subkey K5 mixing
+}
+
+void SPNcihper0(int plaintext[16], int KeyBlock[5][16], int ciphertext[16]) {
+    int T1[16], T2[16], T3[16];
+    for (int round = 0; round < 4; round++) {
+        if (round == 0) mixer(plaintext, T1, KeyBlock[round]); // round 1
+        if (round > 0 && round < 3) { // round 2~3
+            mixer(T1, T1, KeyBlock[round]);
+        }
+        if (round == 3) { // round 4
+            exclusiveOr(T1, KeyBlock[round], T2);
+            substitute(T2, T3, sBox1);
+        }
+    }
+    exclusiveOr(T3, KeyBlock[4], ciphertext); // subkey K5 mixing
 }
 
 void SPNcihper1(int plaintext[16], int KeyBlock[5][16], int ciphertext[16]) {
-    int mixingtext[16], outBlock[16], outPBlock[16], lastBlock[16];
+    int Block[16] = { 0, }, outBlock[16];
     for (int round = 0; round < 4; round++) {
-        mixer1(round, plaintext, outPBlock, KeyBlock[round]); // round 1
+        
+        if(round ==0) mixer1(plaintext, Block, KeyBlock[round]); // round 1
         if (round > 0 && round < 3) { // round 2,3
-            mixer1(round, outPBlock, outBlock, KeyBlock[round]);
+            mixer1(Block, Block, KeyBlock[round]);
         }
         if (round == 3) { // round 4
-            exclusiveOr(outBlock, KeyBlock[round], mixingtext);
+            exclusiveOr(Block, KeyBlock[round], outBlock);
         }
     }
-    exclusiveOr(mixingtext, KeyBlock[4], ciphertext); // subkey K5 mixing
+    exclusiveOr(outBlock, KeyBlock[4], ciphertext); // subkey K5 mixing
 }
-
 int main() {
     printf("\n\n======================[KEY]=======================\n");
     int KEYBlock[5][16];
@@ -115,6 +122,106 @@ int main() {
         puts("");
     }
     printf("==================================================");
+    printf("\n\n======================[평문]======================\n");
+    int plainBlock0[16] = { 0, }, ciphertext0[16]={0,}, mixingSBlock[16], mixingPBlock[16], mixer0[16];
+    printf("[평문]16진수: %X\n", plaintext0);
+    for (int i = 15; i >= 0; i--) {
+        plainBlock0[i] = plaintext0 % 2;
+        plaintext0 = plaintext0 / 2;
+    }
+    printf("[평문]2진수: ");
+    for (int i = 0; i < 16; i++) {
+        printf("%d", plainBlock0[i]);
+    }
+    printf("\n\n=======================[CIPHERBLOCK]====================");
+    printf("\n    [CIPHERTEXT]: ");
+    SPNcihper0(plainBlock0, KEYBlock, ciphertext0);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", ciphertext0[i]);
+    }
+
+    printf("\n\n=======================[ROUND(1)]=======================");
+    printf("\n     {K1 mixing}: ");
+    mixer(plainBlock0, mixer0, KEYBlock[0]);    
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixer0[i]);
+    }
+    printf("\n     [K1 mixing]: ");
+    exclusiveOr(plainBlock0, KEYBlock[0], plainBlock0);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", plainBlock0[i]);
+    }
+    printf("\n[{SUB}K1 mixing]: ");
+    substitute(plainBlock0, mixingSBlock, sBox1);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixingSBlock[i]);
+    }
+    printf("\n[{PER}K1 mixing]: ");
+    permute(mixingSBlock, mixingPBlock, pBox1);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixingPBlock[i]);
+    }
+
+    printf("\n\n=======================[ROUND(2)]=======================");
+    printf("\n     {K2 mixing}: ");
+    mixer(mixingPBlock, mixer0, KEYBlock[1]);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixer0[i]);
+    }
+    printf("\n     [K2 mixing]: ");
+    exclusiveOr(mixingPBlock, KEYBlock[1], mixingPBlock);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixingPBlock[i]);
+    }
+    printf("\n[{SUB}K2 mixing]: ");
+    substitute(mixingPBlock, mixingSBlock, sBox1);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixingSBlock[i]);
+    }
+    printf("\n[{PER}K2 mixing]: ");
+    permute(mixingSBlock, mixingPBlock, pBox1);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixingPBlock[i]);
+    }
+    printf("\n\n=======================[ROUND(3)]=======================");
+    printf("\n     {K3 mixing}: ");
+    mixer(mixingPBlock, mixer0, KEYBlock[2]);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixer0[i]);
+    }
+    printf("\n     [K3 mixing]: ");
+    exclusiveOr(mixingPBlock, KEYBlock[2], mixingPBlock);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixingPBlock[i]);
+    }
+    printf("\n[{SUB}K3 mixing]: ");
+    substitute(mixingPBlock, mixingSBlock, sBox1);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixingSBlock[i]);
+    }
+    printf("\n[{PER}K3 mixing]: ");
+    permute(mixingSBlock, mixingPBlock, pBox1);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixingPBlock[i]);
+    }
+    printf("\n\n=======================[ROUND(4)]=======================");
+    printf("\n     [K4 mixing]: ");
+    exclusiveOr(mixingPBlock, KEYBlock[3], mixingPBlock);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixingPBlock[i]);
+    }
+    printf("\n[{SUB}K4 mixing]: ");
+    substitute(mixingPBlock, mixingSBlock, sBox1);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixingSBlock[i]);
+    }
+    printf("\n\n=======================[KEY5 MIXING]====================");
+    printf("\n     [K5 mixing]: ");
+    exclusiveOr(mixingSBlock, KEYBlock[4], mixingPBlock);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", mixingPBlock[i]);
+    }
+    printf("\n\n ");
     printf("\n\n======================[평문]======================\n");
 
     printf("[평문]16진수: %X\n", plaintext1);
